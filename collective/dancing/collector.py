d056f721000135039858408c98676c7cbd09f42f
nouri <nouri@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-02-05 18:47:05 +0000

    In addition to allowing subscribers to further restrict the search for selection criterias, allow it also for the path a.k.a. location criterium.

--- collective/dancing/collector.py
+++ collective/dancing/collector.py
@@ -11,6 +11,7 @@ import OFS.Folder
 import Products.CMFPlone.interfaces
 import Products.CMFPlone.utils
 import Products.ATContentTypes.criteria.selection
+import Products.ATContentTypes.criteria.path
 import collective.singing.interfaces
 
 class IATCriterionMediator(interface.Interface):
@@ -19,6 +20,11 @@ class IATCriterionMediator(interface.Interface):
         description=u'Field for restriction of criterion',
         schema=zope.schema.interfaces.IField,
         )
+
+    def query_args(value):
+        """Return a dict containing the query parameters that
+        correspond to ``value``.
+        """
 
 class IATSelectionCriterion(interface.Interface):
     pass
@@ -41,6 +47,8 @@ class CollectorContainer(OFS.Folder.Folder):
 @component.adapter(CollectorContainer,
                    zope.app.container.interfaces.IObjectAddedEvent)
 def container_added(container, event):
+    """
+    """
     name = 'default-latest-news'
     container[name] = SmartFolderCollector(
         name, u"Latest news")
@@ -63,8 +71,8 @@ class SmartFolderCollector(OFS.Folder.Folder):
 
     def get_items(self, cue=None, subscription=None):
         topic = self['topic']
-
         query_args = {}
+
         if cue is not None and topic.hasSortCriterion():
             sort_criterion = topic.getSortCriterion()
             query_args[str(sort_criterion.field)] = dict(
@@ -72,8 +80,19 @@ class SmartFolderCollector(OFS.Folder.Folder):
 
         if subscription is not None:
             sdata = collective.singing.interfaces.ICollectorData(subscription)
-            for key, value in sdata.items():
-                query_args[str(key)] = value
+            for cvalue in sdata.values():
+                mediator = None
+                if hasattr(cvalue, '__iter__'):
+                    if len(cvalue):
+                        mediator = cvalue[0].mediator
+                        value = [cvalue.value for cvalue in cvalue]
+                    else:
+                        continue
+                else:
+                    mediator = cvalue.mediator
+                    value = cvalue.value
+
+                query_args.update(mediator.query_args(value))
 
         items = topic.queryCatalog(full_objects=True, **query_args)
         return items, DateTime.DateTime() + 0.005
@@ -117,6 +136,22 @@ class SmartFolderCollector(OFS.Folder.Folder):
 def sfc_added(sfc, event):
     sfc.added()
 
+class CriterionValue(object):
+    def __init__(self, value, mediator):
+        self.value = value
+        self.mediator = mediator
+
+    def __repr__(self):
+        return "<CriterionValue with value %r, mediated by %r>" % (
+            self.value, self.mediator)
+
+def _choice_field(criterion, vocabulary):
+    return schema.Choice(
+        __name__=criterion.Field(),
+        title=zope.i18nmessageid.Message(
+            unicode(criterion.shortDesc), domain='collective.dancing'),
+        vocabulary=vocabulary)
+
 class SelectionCriterionMediator(object):
     component.adapts(IATSelectionCriterion)
     interface.implements(IATCriterionMediator)
@@ -129,9 +164,32 @@ class SelectionCriterionMediator(object):
         dl = self.criterion.getCurrentValues()
         values = self.criterion.Value()
         vocabulary = zope.schema.vocabulary.SimpleVocabulary.fromItems(
-            [(v, dl.getValue(v)) for v in values])
-        return schema.Choice(
-            __name__=self.criterion.field,
-            title=zope.i18nmessageid.Message(
-                unicode(self.criterion.shortDesc), domain='collective.dancing'),
-            vocabulary=vocabulary)
+            [(v, CriterionValue(dl.getValue(v), self)) for v in values])
+        return _choice_field(self.criterion, vocabulary)
+
+    def query_args(self, value):
+        return {self.criterion.Field(): value}
+
+class PathCriterionMediator(object):
+    component.adapts(Products.ATContentTypes.criteria.path.ATPathCriterion)
+    interface.implements(IATCriterionMediator)
+
+    def __init__(self, criterion):
+        self.criterion = criterion
+
+    @property
+    def field(self):
+        terms = []
+        for folder in self.criterion.Value():
+            path = '/'.join(folder.getPhysicalPath())
+            term = zope.schema.vocabulary.SimpleTerm(
+                value=CriterionValue(path, self),
+                title=unicode(folder.Title(), 'UTF-8'))
+            terms.append(term)
+
+        vocabulary = zope.schema.vocabulary.SimpleVocabulary(terms)
+        return _choice_field(self.criterion, vocabulary)
+
+    def query_args(self, value):
+        depth = self.criterion.Recurse() and -1 or 1
+        return {self.criterion.Field(): dict(query=value, depth=depth)}
