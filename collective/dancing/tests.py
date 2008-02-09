9268b5c510a4ef4da1cd1d3507f4dc23cfe812b0
nouri <nouri@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-02-09 17:11:58 +0000

    Tests for the composer module

--- collective/dancing/tests.py
+++ collective/dancing/tests.py
@@ -42,6 +42,11 @@ def test_suite():
             ),
 
         ztc.ZopeDocFileSuite(
+            'composer.txt',
+            test_class=ptc.PloneTestCase,
+            ),
+
+        ztc.ZopeDocFileSuite(
             'browser.txt',
             test_class=ptc.FunctionalTestCase,
             ),
