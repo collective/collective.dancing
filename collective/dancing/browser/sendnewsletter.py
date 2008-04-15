620fa71dba305faed03d260d5d2bd6bc70a30e09
nouri <nouri@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-04-15 13:26:39 +0000

    Poor man's preview for the send as newsletter form.

--- collective/dancing/browser/sendnewsletter.py
+++ collective/dancing/browser/sendnewsletter.py
@@ -3,6 +3,7 @@ import datetime
 from zope import schema
 from zope.app.pagetemplate import viewpagetemplatefile
 from z3c.form import form
+from z3c.form import subform
 from z3c.form import field
 from z3c.form import button
 from Products.Five import BrowserView
@@ -30,6 +31,13 @@ class SendAsNewsletterForm(form.Form):
             title=_(u"Include collector items"),
             default=True,
             ),
+        schema.TextLine(
+            __name__='address',
+            title=_(u"Address to send the preview to"),
+            description=_(
+                u"This is only required if you click 'Send preview' below"),
+            required=False,
+            ),
         )
 
     @button.buttonAndHandler(_('Send'), name='send')
@@ -50,9 +58,39 @@ class SendAsNewsletterForm(form.Form):
                 include_collector_items)
             if channel.scheduler is not None and include_collector_items:
                 channel.scheduler.triggered_last = datetime.datetime.now()
-            channel.queue.dispatch()
 
         self.status = _(u"${num} messages queued.", mapping=dict(num=queued))
+
+    @button.buttonAndHandler(_('Send preview'), name='preview')
+    def handle_preview(self, action):
+        data, errors = self.extractData()
+        if errors:
+            self.status = form.EditForm.formErrorsMessage
+            return
+        channels = data['channels']
+        include_collector_items = data['include_collector_items']
+        address = data['address']
+
+        queued = 0
+        for channel in channels:
+            subs = channel.subscriptions.query(key=address)
+            if len(subs) == 0:
+                self.status = _(
+                    u"${address} is not subscribed to ${channel}.",
+                    mapping=dict(address=address, channel=channel.title))
+                break
+
+            for sub in subs:
+                collective.singing.scheduler.render_message(
+                    channel,
+                    self.request,
+                    sub,
+                    (FullFormatWrapper(self.context),),                
+                    include_collector_items)
+                queued += 1
+        if queued:
+            self.status = _(
+                u"${num} messages queued.", mapping=dict(num=queued))
 
 class SendAsNewsletterView(BrowserView):
     __call__ = ViewPageTemplateFile('controlpanel.pt')
