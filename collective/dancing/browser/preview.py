ac8427c041881ee8f30266b604d1014213979b5e
mborch <mborch@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-05-19 17:26:13 +0000

    Reimplemented preview function so it more accurately displays what would be sent.

--- collective/dancing/browser/preview.py
+++ collective/dancing/browser/preview.py
@@ -1,15 +1,62 @@
+from zope import interface
 from zope import component
+from zope import schema
 
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile 
 
-from collective.dancing.interfaces import IFullFormatter
+from collective.dancing.composer import FullFormatWrapper
+from collective.singing.channel import lookup
+from collective.singing.interfaces import ISubscription
+from collective.singing.scheduler import render_message
 
+class PreviewSubscription(object):
+    interface.implements(ISubscription)
+
+    secret = u""
+
+    collector_data = {}
+    format = 'html'
+    
+    metadata = {
+        'format': format,
+        }
+
+    def __init__(self, channel):
+        self.channel = channel
+
+        composer = self.channel.composers[self.format]
+        
+        # set default composer data
+        self.composer_data = dict(
+            (name, field.default) \
+            for name, field in schema.getFields(composer.schema).items())
+        
 class PreviewNewsletterView(BrowserView):
     template = ViewPageTemplateFile("preview.pt")
     
-    def __call__(self):
-        view = component.getMultiAdapter(
-            (self.context, self.request), IFullFormatter, name='html')
+    def __call__(self, name=None, include_collector_items=False):
+        assert name is not None
 
-        return self.template(content=view(), title=self.context.title_or_id())
+        channel = lookup(name)
+        sub = PreviewSubscription(channel)
+        
+        message = render_message(
+            channel,
+            self.request,
+            sub,
+            (FullFormatWrapper(self.context),),                
+            bool(include_collector_items))
+
+        # pull message out of hat
+        channel.queue[message.status].pull(-1)
+        
+        # walk message, decoding HTML payload
+        for part in message.payload.walk():
+            if part.get_content_type() == 'text/html':
+                html = part.get_payload(decode=True)
+                break
+        else:
+            raise ValueErrorr("Message does not contain a 'text/html' part.")
+            
+        return self.template(content=html, title=self.context.title_or_id())
--- collective/dancing/browser/sendnewsletter.py
+++ collective/dancing/browser/sendnewsletter.py
@@ -1,11 +1,15 @@
 import datetime
 
+from zope import interface
 from zope import schema
+
 from zope.app.pagetemplate import viewpagetemplatefile
+
 from z3c.form import form
 from z3c.form import subform
 from z3c.form import field
 from z3c.form import button
+from z3c.form import validator
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.z3cform import z2
@@ -63,8 +67,23 @@ class SendAsNewsletterForm(form.Form):
 
     @button.buttonAndHandler(_('Show preview'), name='show_preview')
     def handle_show_preview(self, action):
-        self.request.response.redirect(self.context.absolute_url()+\
-                                       '/preview-newsletter.html')
+        data, errors = self.extractData()
+        if errors:
+            self.status = form.EditForm.formErrorsMessage
+            return
+
+        channels = data['channels']
+        if len(channels) != 1:
+            self.status = _(u"Please select precisely one channel for preview.")
+            return
+
+        name = tuple(channels)[0].name
+        include_collector_items = data['include_collector_items']
+        
+        self.request.response.redirect(
+            self.context.absolute_url()+\
+            '/preview-newsletter.html?name=%s&include_collector_items=%d' % \
+            (name, int(bool(include_collector_items))))
 
     @button.buttonAndHandler(_('Send preview'), name='preview')
     def handle_preview(self, action):
