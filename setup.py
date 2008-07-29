3457720809db69c033bd838540d2e2775e7567d0
nouri <nouri@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-07-29 15:16:48 +0000

    Bump version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.5.0'
+version = '0.5.1'
 
 def read(*pathnames):
     return open(os.path.join(os.path.dirname(__file__), *pathnames)).read()
