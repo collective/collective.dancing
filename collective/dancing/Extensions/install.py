ee1f42ddbbe17856a357e792c16674da866f6fe4
toutpt <toutpt@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-10-09 08:47:22 +0000

    remove Extensions

--- collective/dancing/Extensions/install.py
+++
@@ -1,6 +0,0 @@
-def beforeUninstall(portal, reinstall, product, cascade):
-    """Don't remove our utilities on reinstall, thank you!"""
-    if reinstall:
-        return '', filter(lambda c: c not in ('utilities', 'adapters'), cascade)
-    else:
-        return '', cascade
