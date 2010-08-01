9d6a73ef7c8850f17c4f7da0ee0481fed8524c1d
regebro <regebro@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-08-01 07:24:31 +0000

    Double dev versions.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 def read(*pathnames):
     return open(os.path.join(os.path.dirname(__file__), *pathnames)).read()
 
-version = '0.9.1dev'
+version = '0.9.1'
 
 setup(name='collective.dancing',
       version=version,
