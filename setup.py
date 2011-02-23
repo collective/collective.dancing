d9b40e93ddff8f0a6a38fbaadb0e4798955add80
mborch <mborch@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-02-23 20:06:39 +0000

    Version bump.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 def read(name):
     return open(name).read()
 
-version = '0.9.7'
+version = '0.9.8-dev'
 
 setup(name='collective.dancing',
       version=version,
