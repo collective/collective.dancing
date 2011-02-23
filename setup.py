506a813147b0d5a5afa645ee1c52e05e0a9065da
mborch <mborch@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-02-23 20:05:11 +0000

    Support acquisition-aware channels, untested at present.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 def read(name):
     return open(name).read()
 
-version = '0.9.6'
+version = '0.9.7'
 
 setup(name='collective.dancing',
       version=version,
