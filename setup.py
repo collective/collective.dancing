d772c07add0da7d51c5cabcde861bfcc28b7100b
hannosch <hannosch@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-07-22 09:56:47 +0000

    vb

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 def read(name):
     return open(name).read()
 
-version = '0.9.9'
+version = '1.0dev'
 
 setup(name='collective.dancing',
       version=version,
