c012f08317a94cd120185284723d764c3994f157
Andrea Cecchi <andrea.cecchi85@gmail.com>
2014-05-19 15:13:01 +0200

    add unsubscription event

--- collective/dancing/events.py
+++ collective/dancing/events.py
@@ -14,3 +14,12 @@ class ConfirmSubscriptionEvent(zope.component.interfaces.ObjectEvent):
     def __init__(self, channel, subscription):
         super(ConfirmSubscriptionEvent, self).__init__(channel)
         self.subscription = subscription
+
+
+class IConfirmUnsubscriptionEvent(zope.component.interfaces.IObjectEvent):
+    """A subscription has been removed"""
+
+
+class ConfirmUnsubscriptionEvent(zope.component.interfaces.ObjectEvent):
+    """A subscription has been removed"""
+    implements(IConfirmUnsubscriptionEvent)
