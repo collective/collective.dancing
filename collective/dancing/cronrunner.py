4ad89c9c46a454c26faee2b365d7a0b99a35d94e
Adam Terrey <adam@pretaweb.com>
2014-07-09 15:44:22 +0700

    added cronrunner

--- collective/dancing/cronrunner.py
+++ collective/dancing/cronrunner.py
@@ -1,6 +1,21 @@
 
 
+from AccessControl.SecurityManagement import newSecurityManager
+from AccessControl.SecurityManager import setSecurityPolicy 
+from Testing.makerequest import makerequest 
+from Products.CMFCore.tests.base.security import PermissiveSecurityPolicy, OmnipotentUser
+
+
 from collective.cron import crontab
 class SingingCronJob(crontab.Runner):
     def run(self):
-        print "foo"
+
+        portal = self.context
+
+        # not quite sure where to get a requet from
+        newSecurityManager(None, OmnipotentUser().__of__(portal.acl_users))
+        portal = makerequest(portal)
+        utilsview = portal.unrestrictedTraverse("@@dancing.utils")
+
+        import pdb; pdb.set_trace()
+        utilsview._tick_and_dispatch()
