045166d77de89d41c7b7b7d86df221e015112fb1
Ivan Teoh <ivan.teoh@gmail.com>
2014-07-22 13:49:58 +1000

    super first then set the metadata.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(*paths):
     return open(os.path.join(os.path.dirname(__file__), *paths)).read()
 
 
-version = '1.0dev1'
+version = '1.0dev2'
 
 setup(name='collective.dancing',
       version=version,
