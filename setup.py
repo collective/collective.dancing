ac8e417932c27c858064cf13b61ef6225be70669
Ivan Teoh <ivan.teoh@gmail.com>
2015-04-20 11:15:48 +1000

    Up the dev version.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(*paths):
     return open(os.path.join(os.path.dirname(__file__), *paths)).read()
 
 
-version = '1.0dev8'
+version = '1.0dev9'
 
 setup(name='collective.dancing',
       version=version,
